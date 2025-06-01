b7b5e8cb3bb70474518b03d879ae58efa39ae0f8
Viriyadhi <viriyadhitjan@gmail.com>
2025-06-02 01:29:45 +0800

    feat[VS]: adding footer to every pages

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+     <footer class="footer">
+        <button class="footer-button" aria-label="Create new post">
+          <img class="footer-img" src="/src/assets/plus.png" alt="" srcset="" />
+        </button>
+      </footer>
+    `;
+  }
+}
+customElements.define("app-footer", Footer);
