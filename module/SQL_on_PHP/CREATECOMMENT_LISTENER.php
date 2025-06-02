f2f451c179f7ab2c9105c5c781937e25351ff659
akamio <wiboworuimakaio@gmail.com>
2025-06-02 21:40:00 +0700

    CREATECOMMENT 1.0

---
+++ module/SQL_on_PHP/CREATECOMMENT_LISTENER.php
@@ -0,0 +1,33 @@
+<?php
+    include_once __DIR__."/HEADER.php";
+
+    if(isset($_POST['insert_Comment'])) insert_Comment(
+        $_POST['postId'],
+        $_POST['parentId'],
+        $_POST['comment']
+    );
+
+
+function insert_Comment(
+    $comment_postId,
+    $comment_parentId,
+    $comment_comment
+){  
+    $insertComment = getPDO()->prepare('
+        INSERT INTO comment_t (`creatorId`,`postId`,`parentId`,`comment`)
+        VALUES(:creatorId,:postId,:parentId,:comment)
+    ');      
+    $insertComment->bindValue(':creatorId',$_SESSION['uid']);
+    $insertComment->bindValue(':postId',$comment_postId);
+    $insertComment->bindValue(':parentId',$comment_parentId);
+    $insertComment->bindValue(':comment',$comment_comment);
+    
+    try{
+        $insertComment->execute();
+        echo '<body style="--code:success"></body>';
+    } catch(PDOException $e){
+        echo '<body style="--code:\''.$e->getMessage() .'\'"></body>';
+        
+    }
+ }
+?>
