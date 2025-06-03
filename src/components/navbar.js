696d7cda1444bdeb19c7c12a9375a666ac095383
Viriyadhi <viriyadhitjan@gmail.com>
2025-06-03 11:21:08 +0800

    feat[VS]: flexible nav

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,74 @@
 class Navbar extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `
-    <nav style="position:relative;">
-      <h1>LetMeKnow</h1>
-      <form>
-        <input class="search" type="text" placeholder="Search..." />
-      </form>
-      <button data-modal-target=""> 
-      <img id="authBtn" src="/src/assets/circle.png" alt="Login/Signup" style="cursor:pointer;" />
-      </button> 
+    const page = this.getAttribute("data-page");
+    this.classList.add(`navbar-${page}`);
+    let navbarContent;
+    console.log(this.classList);
+
+    switch (page) {
+      case "forum-nav":
+        navbarContent = `
+        <nav >
+          <button  class="back-button ">
+          <img id="back-btn" src="/src/assets/back.png" alt="Back"  />
+          </button>  
+          <h1>LetMeKnow</h1>
+          <form>
+            <input class="search" type="text" placeholder="Search Forum..." />
+          </form>
+          <button class="profile-btn" data-modal-target=""> 
+          <img id="" src="/src/assets/circle.png" alt="Login/Signup" />
+          </button>
+        </nav>
+        <div id="authModal" class="modal hidden"></div>
+      `;
+        break;
+
+      case "post-nav" || "login-nav" || "signup-nav":
+        navbarContent = `
+        <nav >
+          <button  class="back-button ">
+        <img id="back-btn" src="/src/assets/back.png" alt="Back"  />
+          </button>  
+          <h1>LetMeKnow</h1>
+          <form>
+            <input class="search" type="text" placeholder="Search Forum..." />
+          </form>
+          <button class="profile-btn" data-modal-target=""> 
+          <img  src="/src/assets/circle.png" alt="Login/Signup" />
+          </button>
+        </nav>
+        <div id="authModal" class="modal hidden"></div>
+      `;
+        break;
+
+      case "account-nav":
+        navbarContent = `
+        <nav >
+          <button  class="back-button ">
+        <img id="back-btn" src="/src/assets/back.png" alt="Back"  />
+          </button>  
+          <h1>LetMeKnow</h1>
+        </nav>
+      `;
+        break;
+
+      default:
+        navbarContent = `
+          <nav >
+        <h1>LetMeKnow</h1>
+        <form>
+          <input class="search" type="text" placeholder="Search..." />
+        </form>
+        <button class="profile-btn"  data-modal-target=""> 
+        <img  src="/src/assets/circle.png" alt="Login/Signup" />
+        </button>
       </nav>
-    <div id="authModal" class="modal hidden"></div>
-    `;
+      <div id="authModal" class="modal hidden"></div>
+        `;
+        break;
+    }
+    this.innerHTML = navbarContent;
   }
 }
 
