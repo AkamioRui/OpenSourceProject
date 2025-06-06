c4b693a55b6e1b0556f68c4624ca12f87b066af4
akamio <wiboworuimakaio@gmail.com>
2025-06-06 18:18:48 +0700

    added AdminGuide.md and installation.md

--- module/SQL_on_PHP/test/AAAtest_host.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once __DIR__."/HEADER.php";
-    $_SESSION['uid'] = 3;
-    
-
-
-    echo file_get_contents('AAA.html');
-?>
-
--- module/SQL_on_PHP/test/test.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once 'test.html';
-  echo '<button onclick="foo(1,4)">here</button>';
-?>
